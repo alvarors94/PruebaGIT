c0a3ea8b8bf754bc971befd7e817c86a129d0a1e
alvarors94 <ruedasolano.alvaro5@gmail.com>
2024-07-01 11:12:34 +0200

    modificando linea 11

---
+++ cosas.py
@@ -0,0 +1,3 @@
+from prueba import hola
+
+hola()
--- prueba.py
+++ prueba.py
@@ -1,9 +1,12 @@
 prueba = "texto"
 
 
-print("hoala k paza")
+print("hoala k paza, estoy en el nuevo ramaretazo")
 
 print(2+2)
 
 
 print("otrac osa")
+
+def hola():
+    return True
